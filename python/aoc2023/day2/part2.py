9696f989693cb7b7478d21a9548ddb2e3b3105fc
Mietzsche <mitch_hollander@live.com>
2023-12-02 12:31:59 -0600

    Completed day2.2

--- python/aoc2023/day2/part2.py
+++ python/aoc2023/day2/part2.py
@@ -1,5 +1,15 @@
-from typing import List
+from typing import Dict, List
+
+import numpy as np
 
 
-def main(data: List[str]):
-    pass
+def main(data: Dict[int, List[Dict[str, int]]]):
+    sum = 0
+    colors = ["red", "green", "blue"]
+    for id, sets in data.items():
+        print(id, sets)
+        max_seen = {
+            color: max([draw.get(color, 0) for draw in sets]) for color in colors
+        }
+        sum += np.product(list(max_seen.values()))
+    return sum
