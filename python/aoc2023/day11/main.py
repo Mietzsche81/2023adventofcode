82155475b5a72d18eab32fd556eaa2dbfcc17d3a
Mietzsche <mitch_hollander@live.com>
2023-12-11 00:21:43 -0600

    Day11 faster logic.

--- python/aoc2023/day11/main.py
+++ python/aoc2023/day11/main.py
@@ -8,15 +8,10 @@ def main(galaxies, empty_row, empty_col, empty_size=1):
     for x, y in itertools.combinations(range(len(galaxies)), 2):
         lower, upper = sorted([galaxies[x], galaxies[y]], key=lambda g: g[0])
         left, right = sorted([galaxies[x], galaxies[y]], key=lambda g: g[1])
-        for j in range(left[1] + 1, right[1] + 1):
-            if j in empty_col:
-                distance += empty_size
-            else:
-                distance += 1
-        for i in range(lower[0] + 1, upper[0] + 1):
-            if i in empty_row:
-                distance += empty_size
-            else:
-                distance += 1
+        vertical = (range(lower[0] + 1, upper[0] + 1) == empty_row).any(axis=0)
+        horizontal = (range(left[1] + 1, right[1] + 1) == empty_col).any(axis=0)
+
+        distance += vertical.sum() * empty_size + (~vertical).sum()
+        distance += horizontal.sum() * empty_size + (~horizontal).sum()
 
     return distance
